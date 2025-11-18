8810757411390c24b6ff46f55cabe9d8ddde2ff5
evgzor <>
2025-11-18 21:33:01 +0300

    First Try

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +14,100 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size < 1 {
+		return []int{}
+	}
+
+	elements := make([]int, size)
+	var randSource = rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(randSource)
+	for i := range size {
+		elements[i] = rng.Int()
+	}
+
+	return elements
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+
+	result := data[0]
+	for _, element := range data[1:] {
+		if element > result {
+			result = element
+		}
+	}
+	return result
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+	if len(data)%CHUNKS != 0 {
+		return 0
+	}
+	maxElements := make([]int, CHUNKS)
+
+	var wg sync.WaitGroup
+	for i := range CHUNKS {
+		wg.Add(1) // инкрементируем счётчик перед запуском горутины
+
+		size := len(data)
+		startIdx := i * size / CHUNKS
+		endIdx := i*size/CHUNKS + size/CHUNKS
+		go func(idx int, elements []int) {
+			// уменьшаем счётчик, когда горутина завершает работу
+			defer wg.Done()
+
+			result := elements[0]
+			for _, element := range elements[1:] {
+				if element > result {
+					result = element
+				}
+			}
+			// захватили индекс ... и не нужна нам каналы и мютексы
+			maxElements[idx] = result
+
+		}(i, data[startIdx:endIdx])
+	}
+	wg.Wait()
+
+	result := maxElements[0]
+	for _, element := range maxElements[1:] {
+		if element > result {
+			result = element
+		}
+	}
+
+	return result
 }
 
 func main() {
 	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	elements := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	timeStart := time.Now()
+	max := maximum(elements)
+	timeEnd := time.Now()
+
+	elapsed := timeEnd.Sub(timeStart).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
 	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	timeStart = time.Now()
+	max = maxChunks(elements)
+	timeEnd = time.Now()
+	elapsed = timeEnd.Sub(timeStart).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,43 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+	require.Len(t, generateRandomElements(-10), 0)
+	require.Len(t, generateRandomElements(0), 0)
+	require.Len(t, generateRandomElements(1), 1)
+}
+
+func TestMaximum(t *testing.T) {
+	var randSource = rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(randSource).Int()
+	assert.Equal(t, rng, maximum([]int{rng}))
+	assert.Equal(t, 0, maximum(nil))
+	assert.Equal(t, maximum([]int{0, 1, 1, 1, 1, 0}), 1)
+	assert.Equal(t, maximum([]int{0, -1, 1, 1, -1000000, 0}), 1)
+}
+
+func TestMaximumChunks(t *testing.T) {
+	var randSource = rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(randSource).Int() + 1
+	assert.Equal(t, rng, maxChunks([]int{rng}))
+	assert.Equal(t, 0, maxChunks(nil))
+	assert.Equal(t, maxChunks([]int{0, 1, 1, 1, 1, 0, 1, rng}), rng)
+	assert.Equal(t, maxChunks([]int{0, -1, -10, -1, -1000000, 0, 0, 0}), 0)
+}
+
+func TestBoth(t *testing.T) {
+	var randSource = rand.NewSource(time.Now().UnixNano())
+	randomNumber := randSource.Int63()%10 + 1
+	for range randomNumber {
+		elements := generateRandomElements(SIZE)
+		require.Equal(t, maximum(elements), maxChunks(elements))
+	}
+}
